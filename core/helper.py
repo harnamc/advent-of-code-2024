a3c1b704c42859233266192652c0035930de6ea7
Harnam Chana <harnamsc@amazon.co.uk>
2024-12-09 11:52:24 +0000

    Complete day 3 part 2

--- core/helper.py
+++ core/helper.py
@@ -1,3 +1,9 @@
 def read_input_file_to_tuple_of_ints(file_path):
     with open(file_path, "r") as file:
         return [tuple(map(int, line.split())) for line in file]
+
+
+def read_input_file(file_path):
+    with open(file_path, "r") as file:
+        # Need the `line.strip()` to remove the \n that gets added by file.readlines()
+        return [line.strip() for line in file.readlines()]
--- day_3/part_2.py
+++ day_3/part_2.py
@@ -5,10 +5,10 @@ from core.helper import read_input_file
 from core.logger import get_logger
 
 logger = get_logger("day_3_part_1")
-mul_instruction_regex = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+mul_instruction_regex = r"(?:don\'t\(\).*?do\(\))|mul\((\d{1,3}),(\d{1,3})\)"
 input = read_input_file(f"{Path(__file__).parent}/input.txt")
 input_string = "".join(input)
 matches = re.findall(mul_instruction_regex, input_string)
-total = sum([int(x) * int(y) for x, y in matches])
+total = sum([int(x) * int(y) for x, y in matches if len(x) > 0 and len(y) > 0])
 
 logger.info(f"Total: {total}")
